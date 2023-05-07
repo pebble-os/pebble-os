7f3f08d4aefca0447f5d21580ea4747251272296
Austin Meadows <austin@slowp.ke>
2023-05-07 19:30:50 -0400

    🧼 a lot of cleanup and fixes

--- src/Executables/SECURITY.ps1
+++
@@ -1,8 +0,0 @@
-#Disable NetBIOS by updating Registry
-#http://blog.dbsnet.fr/disable-netbios-with-powershell#:~:text=Disabling%20NetBIOS%20over%20TCP%2FIP,connection%2C%20then%20set%20NetbiosOptions%20%3D%202
-$key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-Get-ChildItem $key | ForEach-Object {
-	Write-Host("Modify $key\$($_.pschildname)")
-	$NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
-	Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
-}
